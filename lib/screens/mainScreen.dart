b4e9d5a8fcd022ba3635b58d1cb4ba530be793ad
Yousuf Jamal <yousuf.jamal005@gmail.com>
2025-04-24 00:06:11 +0500

    FAB & 3rd screen rem

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -5,22 +5,32 @@ import 'package:flutter/material.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:mobileappdevintern_test/utils/StatisticsCard.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<MainScreen> createState() => _MainScreenState();
+}
 
+class _MainScreenState extends State<MainScreen> {
+  int _rowsPerPage = 10;
+  int _currentPage = 1;
+  final int _totalRows = 30;
+
+  void navigatetoFormscreen(BuildContext context) {}
+
+  @override
+  Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.only(left: 20,right: 20),
+        padding: EdgeInsets.only(left: 20, right: 20),
         child: ListView(
           children: [
-            SizedBox(height: screenHeight * 0.04,),
+            SizedBox(height: screenHeight * 0.04),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -36,20 +46,20 @@ class MainScreen extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Icon(Icons.home,color: Colors.blue,),
+                    Icon(Icons.home, color: Colors.blue),
                     Padding(
                       padding: EdgeInsets.only(left: 12),
-                      child: Icon(Icons.bar_chart,color: Colors.black),
+                      child: Icon(Icons.bar_chart, color: Colors.black),
                     ),
                     Padding(
                       padding: EdgeInsets.only(left: 12),
-                      child: Icon(Icons.logout,color: Colors.black,),
+                      child: Icon(Icons.logout, color: Colors.black),
                     ),
                   ],
-                )
+                ),
               ],
             ),
-            SizedBox(height: screenHeight * 0.04,),
+            SizedBox(height: screenHeight * 0.04),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -65,14 +75,11 @@ class MainScreen extends StatelessWidget {
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(18),
-                    border: Border.all(
-                      color: Colors.blue,
-                      width: 2,
-                    )
+                    border: Border.all(color: Colors.blue, width: 2),
                   ),
                   height: 30,
                   child: Padding(
-                    padding: EdgeInsets.only(left: 16,right: 16),
+                    padding: EdgeInsets.only(left: 16, right: 16),
                     child: Row(
                       children: [
                         Text(
@@ -85,19 +92,125 @@ class MainScreen extends StatelessWidget {
                         ),
                         Padding(
                           padding: const EdgeInsets.only(left: 4),
-                          child: Icon(CupertinoIcons.chevron_down,color: Colors.blue,size: 14,),
-                        )
+                          child: Icon(
+                            CupertinoIcons.chevron_down,
+                            color: Colors.blue,
+                            size: 14,
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                )
+                ),
               ],
             ),
-            SizedBox(height: screenHeight * 0.02,),
+            SizedBox(height: screenHeight * 0.02),
             Statisticscard(),
+            SizedBox(height: screenHeight * 0.04),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Past Visits',
+                  style: TextStyle(
+                    fontFamily: 'Noyh-Medium',
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(5, 71, 120, 1),
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.white,
+                        offset: Offset(4, 2),
+                      )
+                    ],
+                  ),
+                  child: IconButton(
+                      onPressed: () {
+                        navigatetoFormscreen(context);
+                      },
+                      icon: Icon(Icons.add,color: Colors.white,)),
+                ),
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            Container(
+              width: screenWidth,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: DataTable(
+                columnSpacing: 16,
+                horizontalMargin: 12,
+                columns: [
+                  DataColumn(label: Text('Date', style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataColumn(label: Text('Market', style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataColumn(label: Text('Store Name', style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataColumn(label: Center(child: Text('Perfect Score', style: TextStyle(fontWeight: FontWeight.bold)))),
+                ],
+                rows: [
+                  _buildDataRow('18-03-24', 'BAH', 'Lorem Ipsum', 77),
+                  _buildDataRow('17-03-24', 'KWT', 'Lorem Ipsum', 90),
+                  _buildDataRow('17-03-24', 'MOR', 'Lorem Ipsum', 74),
+                  _buildDataRow('20-03-24', 'UAE', 'Lorem Ipsum', 80),
+                  _buildDataRow('18-03-24', 'QTR', 'Lorem Ipsum', 60),
+                  _buildDataRow('17-03-24', 'KSA', 'Lorem Ipsum', 56),
+                  _buildDataRow('17-03-24', 'QMIN', 'Lorem Ipsum', 89),
+                  _buildDataRow('18-03-24', 'BAH', 'Lorem Ipsum', 77),
+                  _buildDataRow('17-03-24', 'KWT', 'Lorem Ipsum', 90),
+                  _buildDataRow('17-03-24', 'MOR', 'Lorem Ipsum', 74),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Rows per page:',
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+                Text(
+                  '10'
+                ),
+                Icon(Icons.arrow_drop_down),
+                Text(
+                  '1-10 of 30',
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.chevron_left, color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.chevron_right, color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 100), // Extra space for FAB
           ],
         ),
       ),
     );
+  }
+
+  DataRow _buildDataRow(String date, String market, String store, int score) {
+    return DataRow(cells: [
+      DataCell(Text(date)),
+      DataCell(Text(market)),
+      DataCell(Text(store)),
+      DataCell(Center(child: Text(score.toString()))),
+    ]);
   }
 }
--- lib/utils/StatisticsCard.dart
+++ lib/utils/StatisticsCard.dart
@@ -15,7 +15,7 @@ class Statisticscard extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: 200,
+          height: 150,
           width: screenWidth,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
@@ -39,13 +39,196 @@ class Statisticscard extends StatelessWidget {
                 progressColor: Colors.blue,
                 circularStrokeCap: CircularStrokeCap.round,
               ),
-              Text(
-                'Average Perfect Store Score',
-                style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.only(top: 6.0),
+                child: Text(
+                  'Average Perfect Store Score',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
                 ),
               )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top:14.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.grey.shade200
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '60%',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Color.fromRGBO(1, 59, 83, 1.0),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 6.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Planogram',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            'Compliance',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400
+                            ),
+                          ),
+                        ],
+                      )
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.grey.shade200
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '58%',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Color.fromRGBO(1, 59, 83, 1.0),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Share of',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'Shelf (SOS)',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400
+                              ),
+                            ),
+                          ],
+                        )
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.grey.shade200
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '62%',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Color.fromRGBO(1, 59, 83, 1.0),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Must Stock',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'List',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400
+                              ),
+                            ),
+                          ],
+                        )
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.grey.shade200
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '63%',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Color.fromRGBO(1, 59, 83, 1.0),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'On Shelf',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'Availability',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400
+                              ),
+                            ),
+                          ],
+                        )
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         )
