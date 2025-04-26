cf42108aefc44865df92e983d3b34c9f43149e68
Yousuf Jamal <yousuf.jamal005@gmail.com>
2025-04-26 15:47:15 +0500

    final push v2

--- lib/screens/landingScreen.dart
+++ lib/screens/landingScreen.dart
@@ -17,7 +17,7 @@ class _LandingScreenState extends State<LandingScreen> {
   {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         PageRouteBuilder(
@@ -38,7 +38,6 @@ class _LandingScreenState extends State<LandingScreen> {
         ),
       );
     });
-
   }
 
   @override
